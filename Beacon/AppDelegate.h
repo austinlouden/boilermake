d48cb9ecad3672d49b82537ff0d222a71139fdea
Austin Louden <austinlouden@gmail.com>
2014-02-07 23:00:09 -0500

    initial commit

---
+++ Beacon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Beacon
+//
+//  Created by Austin Louden on 2/7/14.
+//  Copyright (c) 2014 Austin Louden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
