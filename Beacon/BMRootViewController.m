0ce0dac05c3407429f857fab88670ce6002b4046
Austin Louden <austinlouden@gmail.com>
2014-02-09 06:16:55 -0500

    animations

--- Beacon/BMRootViewController.m
+++ Beacon/BMRootViewController.m
@@ -155,29 +155,149 @@
     
 }
 
+- (CABasicAnimation *)translateAnimationFromPoint:(CGPoint)point1 toPoint:(CGPoint)point2 withDuraction:(float)duration {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+    [animation setDuration:duration];
+    [animation setFromValue:[NSValue valueWithCGPoint:point1]];
+    animation.toValue = [NSValue valueWithCGPoint:point2];
+    return animation;
+}
+
 - (void)animatePeople
 {
+    
     BMPerson *person1 = (BMPerson*)[self.view viewWithTag:41];
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:8.0];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    person1.frame = CGRectMake(1335.0f, 550.0f, PERSON_SIZE, PERSON_SIZE);
-    [UIView commitAnimations];
+    CABasicAnimation *a1 = [self translateAnimationFromPoint:CGPointMake(1365.0, 297.0) toPoint:CGPointMake(1365.0, 557.0) withDuraction:2.0f]; // down
+    CABasicAnimation *a2 = [self translateAnimationFromPoint:CGPointMake(1365.0, 557.0) toPoint:CGPointMake(1585.0, 557.0) withDuraction:2.0f]; // right
+    CABasicAnimation *a3 = [self translateAnimationFromPoint:CGPointMake(1585.0, 557.0) toPoint:CGPointMake(1585.0, 800.0) withDuraction:2.0f]; // down
     
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:3.0];
-    [UIView setAnimationDelay:8.0];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    person1.frame = CGRectMake(1585.0f, 550.0f, PERSON_SIZE, PERSON_SIZE);
-    [UIView commitAnimations];
+    CABasicAnimation *s1 = [self translateAnimationFromPoint:CGPointMake(1585.0, 800.0) toPoint:CGPointMake(1585.0, 800.0) withDuraction:4.0f]; // stay
     
-    /*[UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:3.0];
-    [UIView setAnimationDelay:9.0];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    person1.frame = CGRectMake(1585.0f, 950.0f, PERSON_SIZE, PERSON_SIZE);
-    [UIView commitAnimations];*/
+    CABasicAnimation *b3 = [self translateAnimationFromPoint:CGPointMake(1585.0, 800.0) toPoint:CGPointMake(1585.0, 557.0) withDuraction:2.0f]; // up
+    CABasicAnimation *b2 = [self translateAnimationFromPoint:CGPointMake(1585.0, 557.0) toPoint:CGPointMake(1365.0, 557.0) withDuraction:2.0f]; // left
+    CABasicAnimation *b1 = [self translateAnimationFromPoint:CGPointMake(1365.0, 557.0) toPoint:CGPointMake(1365.0, 297.0) withDuraction:2.0f]; // up
+    a2.beginTime = 2.0f;
+    a3.beginTime = 4.0f;
+    s1.beginTime = 6.0f;
+    b3.beginTime = 10.0f;
+    b2.beginTime = 12.0f;
+    b1.beginTime = 14.0f;
     
+    CAAnimationGroup *anim = [CAAnimationGroup animation];
+    [anim setAnimations:[NSArray arrayWithObjects:a1, a2, a3, s1, b3, b2, b1, nil]];
+    [anim setDuration:16.0];
+    [anim setRemovedOnCompletion:NO];
+    [anim setFillMode:kCAFillModeForwards];
+    [person1.layer addAnimation:anim forKey:nil];
+    
+    ///////////////////
+    
+    BMPerson *person2 = (BMPerson*)[self.view viewWithTag:42];
+    CABasicAnimation *ss0 = [self translateAnimationFromPoint:CGPointMake(1375.0, 800.0) toPoint:CGPointMake(1375.0, 800.0) withDuraction:2.0f]; // stay
+    CABasicAnimation *aa1 = [self translateAnimationFromPoint:CGPointMake(1375.0, 800.0) toPoint:CGPointMake(1375.0, 257.0) withDuraction:7.0f]; // up
+    CABasicAnimation *ss1 = [self translateAnimationFromPoint:CGPointMake(1375.0, 257.0) toPoint:CGPointMake(1375.0, 257.0) withDuraction:5.0f]; // stay
+    CABasicAnimation *bb1 = [self translateAnimationFromPoint:CGPointMake(1375.0, 257.0) toPoint:CGPointMake(1375.0, 800.0) withDuraction:7.0f]; // down
+    aa1.beginTime = 2.0f;
+    ss1.beginTime = 9.0f;
+    bb1.beginTime = 14.0f;
+    
+    CAAnimationGroup *aanim = [CAAnimationGroup animation];
+    [aanim setAnimations:[NSArray arrayWithObjects:ss0, aa1, ss1, bb1, nil]];
+    [aanim setDuration:21.0];
+    [aanim setRemovedOnCompletion:NO];
+    [aanim setFillMode:kCAFillModeForwards];
+    [person2.layer addAnimation:aanim forKey:nil];
+    
+    ///////////////////
+    
+    BMPerson *person3 = (BMPerson*)[self.view viewWithTag:43];
+    CABasicAnimation *sss1 = [self translateAnimationFromPoint:CGPointMake(1800.0, 800.0) toPoint:CGPointMake(1800.0, 800.0) withDuraction:4.0f]; // stay
+    CABasicAnimation *aaa1 = [self translateAnimationFromPoint:CGPointMake(1800.0, 800.0) toPoint:CGPointMake(1800.0, 550.0) withDuraction:4.0f]; // up
+    CABasicAnimation *sss2 = [self translateAnimationFromPoint:CGPointMake(1800.0, 550.0) toPoint:CGPointMake(1800.0, 550.0) withDuraction:4.0f]; // stay
+    CABasicAnimation *aaa2 = [self translateAnimationFromPoint:CGPointMake(1800.0, 550.0) toPoint:CGPointMake(1570.0, 550.0) withDuraction:2.0f]; // left
+    CABasicAnimation *aaa3 = [self translateAnimationFromPoint:CGPointMake(1570.0, 550.0) toPoint:CGPointMake(1570.0, 700.0) withDuraction:2.0f]; // down
+    
+    CABasicAnimation *bbb3 = [self translateAnimationFromPoint:CGPointMake(1570.0, 700.0) toPoint:CGPointMake(1570.0, 550.0) withDuraction:2.0f]; // up
+    CABasicAnimation *bbb2 = [self translateAnimationFromPoint:CGPointMake(1570.0, 550.0) toPoint:CGPointMake(1800.0, 550.0) withDuraction:2.0f]; // right
+    CABasicAnimation *bbb1 = [self translateAnimationFromPoint:CGPointMake(1800.0, 550.0) toPoint:CGPointMake(1800.0, 800.0) withDuraction:4.0f]; // down
+    aaa1.beginTime = 4.0f;
+    sss2.beginTime = 8.0f;
+    aaa2.beginTime = 12.0f;
+    aaa3.beginTime = 14.0f;
+    bbb3.beginTime = 16.0f;
+    bbb2.beginTime = 18.0f;
+    bbb1.beginTime = 20.0f;
+    
+    CAAnimationGroup *aaanim = [CAAnimationGroup animation];
+    [aaanim setAnimations:[NSArray arrayWithObjects:sss1, aaa1, sss2, aaa2, aaa3, bbb3, bbb2, bbb1, nil]];
+    [aaanim setDuration:24.0];
+    [aaanim setRemovedOnCompletion:NO];
+    [aaanim setFillMode:kCAFillModeForwards];
+    [person3.layer addAnimation:aaanim forKey:nil];
+    
+    ////////////////////
+    
+    BMPerson *person4 = (BMPerson*)[self.view viewWithTag:44];
+    CABasicAnimation *aaaa1 = [self translateAnimationFromPoint:CGPointMake(1590.0, 350.0) toPoint:CGPointMake(1590.0, 800.0) withDuraction:4.0f];
+    CABasicAnimation *aaaa2 = [self translateAnimationFromPoint:CGPointMake(1590.0, 800.0) toPoint:CGPointMake(1590.0, 550.0) withDuraction:2.0f];
+    CABasicAnimation *aaaa3 = [self translateAnimationFromPoint:CGPointMake(1590.0, 550.0) toPoint:CGPointMake(1370.0, 550.0) withDuraction:2.0f];
+    CABasicAnimation *aaaa4 = [self translateAnimationFromPoint:CGPointMake(1370.0, 550.0) toPoint:CGPointMake(1370.0, 850.0) withDuraction:2.0f];
+    CABasicAnimation *bbbb1 = [self translateAnimationFromPoint:CGPointMake(1370.0, 850.0) toPoint:CGPointMake(1370.0, 550.0) withDuraction:2.0f];
+    CABasicAnimation *bbbb2 = [self translateAnimationFromPoint:CGPointMake(1370.0, 550.0) toPoint:CGPointMake(1590.0, 550.0) withDuraction:2.0f];
+    CABasicAnimation *bbbb3 = [self translateAnimationFromPoint:CGPointMake(1590.0, 550.0) toPoint:CGPointMake(1590.0, 350.0) withDuraction:2.0f];
+    aaaa2.beginTime = 4.0f;
+    aaaa3.beginTime = 6.0f;
+    aaaa4.beginTime = 8.0f;
+    bbbb1.beginTime = 10.0f;
+    bbbb2.beginTime = 12.0f;
+    bbbb3.beginTime = 14.0f;
+    
+    CAAnimationGroup *aaaanim = [CAAnimationGroup animation];
+    [aaaanim setAnimations:[NSArray arrayWithObjects:aaaa1, aaaa2, aaaa3, aaaa4, bbbb1, bbbb2, bbbb3, nil]];
+    [aaaanim setDuration:16.0];
+    [aaaanim setRemovedOnCompletion:NO];
+    [aaaanim setFillMode:kCAFillModeForwards];
+    [person4.layer addAnimation:aaaanim forKey:nil];
+    
+    ////////////////////////
+    
+    BMPerson *person5 = (BMPerson*)[self.view viewWithTag:45];
+    CABasicAnimation *aaaaa1 = [self translateAnimationFromPoint:CGPointMake(1590.0, 550.0) toPoint:CGPointMake(1800.0, 550.0) withDuraction:3.0f];
+    CABasicAnimation *sssss1 = [self translateAnimationFromPoint:CGPointMake(1800.0, 550.0) toPoint:CGPointMake(1800.0, 550.0) withDuraction:4.0f];
+    CABasicAnimation *aaaaa2 = [self translateAnimationFromPoint:CGPointMake(1800.0, 550.0) toPoint:CGPointMake(1800.0, 350.0) withDuraction:2.0f];
+    CABasicAnimation *sssss2 = [self translateAnimationFromPoint:CGPointMake(1800.0, 350.0) toPoint:CGPointMake(1800.0, 350.0) withDuraction:2.0f];
+    CABasicAnimation *bbbbb2 = [self translateAnimationFromPoint:CGPointMake(1800.0, 350.0) toPoint:CGPointMake(1800.0, 550.0) withDuraction:4.0f];
+    CABasicAnimation *bbbbb1 = [self translateAnimationFromPoint:CGPointMake(1800.0, 550.0) toPoint:CGPointMake(1590.0, 550.0) withDuraction:4.0f];
+    sssss1.beginTime = 3.0f;
+    aaaaa2.beginTime = 7.0f;
+    sssss2.beginTime = 9.0f;
+    bbbbb2.beginTime = 11.0f;
+    bbbbb1.beginTime = 15.0f;
+    
+    CAAnimationGroup *aaaaanim = [CAAnimationGroup animation];
+    [aaaaanim setAnimations:[NSArray arrayWithObjects:aaaaa1, sssss1, aaaaa2, sssss2, bbbbb2, bbbbb1, nil]];
+    [aaaaanim setDuration:19.0];
+    [aaaaanim setRemovedOnCompletion:NO];
+    [aaaaanim setFillMode:kCAFillModeForwards];
+    [person5.layer addAnimation:aaaaanim forKey:nil];
+    
+    ///////////////////////////
+    
+    BMPerson *person6 = (BMPerson*)[self.view viewWithTag:46];
+    CABasicAnimation *ssssss1 = [self translateAnimationFromPoint:CGPointMake(1590.0, 800.0) toPoint:CGPointMake(1590.0, 800.0) withDuraction:1.0f];
+    CABasicAnimation *aaaaaa1 = [self translateAnimationFromPoint:CGPointMake(1590.0, 800.0) toPoint:CGPointMake(1590.0, 550.0) withDuraction:4.0f];
+    CABasicAnimation *ssssss2 = [self translateAnimationFromPoint:CGPointMake(1590.0, 550.0) toPoint:CGPointMake(1590.0, 550.0) withDuraction:4.0f];
+    CABasicAnimation *bbbbbb1 = [self translateAnimationFromPoint:CGPointMake(1590.0, 550.0) toPoint:CGPointMake(1590.0, 800.0) withDuraction:2.0f];
+    aaaaaa1.beginTime = 1.0f;
+    ssssss2.beginTime = 5.0f;
+    bbbbbb1.beginTime = 9.0f;
+    
+    CAAnimationGroup *aaaaaanim = [CAAnimationGroup animation];
+    [aaaaaanim setAnimations:[NSArray arrayWithObjects:ssssss1, aaaaaa1, ssssss2, bbbbbb1, nil]];
+    [aaaaaanim setDuration:11.0];
+    [aaaaaanim setRemovedOnCompletion:NO];
+    [aaaaaanim setFillMode:kCAFillModeForwards];
+    [person6.layer addAnimation:aaaaaanim forKey:nil];
 }
 
 - (void)blastPressed
